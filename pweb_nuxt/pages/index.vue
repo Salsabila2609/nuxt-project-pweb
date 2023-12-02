e567cdf035f92e906505f1f3b36f9a53bfefdebd
Iki Adfi <ikiadfi15@gmail.com>
2023-12-02 22:25:40 +0700

    fix ssg & csr

--- pweb_nuxt/pages/index.vue
+++ pweb_nuxt/pages/index.vue
@@ -10,24 +10,6 @@
       </div>
     </section>
 
-    <section class="book-form" id="book-form">
-      <form action="">
-        <div data-aos="zoom-in" data-aos-delay="150" class="inputBox">
-          <span>you are planning?</span>
-          <input type="text" placeholder="place name" value="" />
-        </div>
-        <div data-aos="zoom-in" data-aos-delay="300" class="inputBox">
-          <span>When looking for ?</span>
-          <input type="date" value="" />
-        </div>
-        <div data-aos="zoom-in" data-aos-delay="450" class="inputBox">
-          <span>Total members?</span>
-          <input type="number" placeholder="number of travelers" value="" />
-        </div>
-        <input data-aos="zoom-in" data-aos-delay="600" type="submit" value="find now" class="btn" />
-      </form>
-    </section>
-
     <section class="about" id="about">
       <div class="video-container" data-aos="fade-right" data-aos-delay="300">
         <video src="../images/about.mp4" muted autoplay loop class="video"></video>
@@ -114,8 +96,8 @@
           <div class="user">
             <img src="../images/pic-1.png" alt="" />
             <div class="info">
-              <h3>john deo</h3>
-              <span>designer</span>
+              <h3>Iki Adfi</h3>
+              <span>Designer</span>
             </div>
           </div>
         </div>
@@ -124,28 +106,28 @@
           <div class="user">
             <img src="../images/pic-2.png" alt="" />
             <div class="info">
-              <h3>john deo</h3>
-              <span>designer</span>
+              <h3>Caca</h3>
+              <span>Copywriter</span>
             </div>
           </div>
         </div>
         <div class="box">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <p>So happy with this website!</p>
           <div class="user">
             <img src="../images/pic-3.png" alt="" />
             <div class="info">
-              <h3>john deo</h3>
-              <span>designer</span>
+              <h3>Hafiz Kusuma</h3>
+              <span>Videografer</span>
             </div>
           </div>
         </div>
         <div class="box">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          <p>This blog inspires and elevates my wanderlust.</p>
           <div class="user">
             <img src="../images/pic-4.png" alt="" />
             <div class="info">
-              <h3>john deo</h3>
-              <span>designer</span>
+              <h3>Ilhan</h3>
+              <span>Staff</span>
             </div>
           </div>
         </div>
@@ -154,9 +136,7 @@
       <div class="content" data-aos="fade-left" data-aos-delay="600">
         <span>testimonials</span>
         <h3>good news from our clients</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda laudantium corporis fugiat quae unde perspiciatis similique ab modi enim consequatur aperiam cumque distinctio facilis sit, debitis possimus asperiores non harum.
-        </p>
+        <p>Celebration Of Diversity And The Beauty That Exists In Every Corner Of The Globe. Whether You're A Seasoned Globetrotter Or A First-Time Explorer, Our Goal Is To Inspire, Inform, And Elevate Your Travel Experiences.</p>
       </div>
     </section>
 
@@ -164,61 +144,10 @@
       <div class="content" data-aos="zoom-in-up" data-aos-delay="300">
         <span>start your adventures</span>
         <h3>Let's Explore World With Us</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum voluptatum praesentium amet quibusdam quam officia suscipit odio.</p>
+        <p>Celebration Of Diversity And The Beauty That Exists In Every Corner Of The Globe. Whether You're A Seasoned Globetrotter Or A First-Time Explorer, Our Goal Is To Inspire, Inform, And Elevate Your Travel Experiences..</p>
         <a href="#book-form" class="btn">book now</a>
       </div>
     </div>
-
-    <section class="blogs" id="blogs">
-      <div class="heading">
-        <span>blogs & posts</span>
-        <h1>Experience in words</h1>
-      </div>
-
-      <div class="box-container">
-        <div class="box" data-aos="fade-up" data-aos-delay="150">
-          <div class="image">
-            <img src="../images/blog-1.jpg" alt="" />
-          </div>
-          <div class="content">
-            <a href="#" class="link">One Mile At A Time.</a>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, natus!</p>
-            <div class="icon">
-              <a href="#"><i class="fas fa-clock"></i> 21st may, 2021</a>
-              <a href="#"><i class="fas fa-user"></i> by admin</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="box" data-aos="fade-up" data-aos-delay="300">
-          <div class="image">
-            <img src="../images/blog-2.jpg" alt="" />
-          </div>
-          <div class="content">
-            <a href="#" class="link">Local Adventurer</a>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, natus!</p>
-            <div class="icon">
-              <a href="#"><i class="fas fa-clock"></i> 21st may, 2021</a>
-              <a href="#"><i class="fas fa-user"></i> by admin</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="box" data-aos="fade-up" data-aos-delay="450">
-          <div class="image">
-            <img src="../images/blog-3.jpg" alt="" />
-          </div>
-          <div class="content">
-            <a href="#" class="link">The Blonde Abroad</a>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, natus!</p>
-            <div class="icon">
-              <a href="#"><i class="fas fa-clock"></i> 21st may, 2021</a>
-              <a href="#"><i class="fas fa-user"></i> by admin</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
     <Footer></Footer>
   </div>
 </template>
